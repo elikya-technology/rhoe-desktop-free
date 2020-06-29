55259a4e68b25b0940825abe9805089b87c5b77d
Stephen Mafole <mafole.loemelah@gmail.com>
2020-06-29 15:45:44 +0200

    added startermaster

---
+++ src/main/java/com/elikya/apps/rhoe/desk/controller/MoreStarterController.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020, Elikya Technology.
+ */
+
+package com.elikya.apps.rhoe.desk.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoreStarterController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
