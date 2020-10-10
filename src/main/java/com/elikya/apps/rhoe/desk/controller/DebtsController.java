080ffc27c20e70ef5c16a6e0f0fed7a92a82dd74
stephen-mafole <stephen-mafole@elikya.tech>
2020-10-10 16:37:15 +0200

    added Debts as title

--- src/main/java/com/elikya/apps/rhoe/desk/controller/DebtsController.java
+++ src/main/java/com/elikya/apps/rhoe/desk/controller/DebtsController.java
@@ -6,6 +6,7 @@ package com.elikya.apps.rhoe.desk.controller;
 
 import com.elikya.apps.rhoe.desk.entity.Payment;
 import com.elikya.apps.rhoe.desk.observers.interfaces.CRUDMaster;
+import com.elikya.apps.rhoe.desk.ui.ControlsHandler;
 import com.elikya.apps.rhoe.desk.ui.Stages;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
@@ -19,6 +20,7 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.net.URL;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
@@ -31,32 +33,37 @@ public class DebtsController implements Initializable, CRUDMaster {
 
     @FXML private Label title;
     @FXML private JFXButton close;
+    @FXML private SplitPane splitPane;
+    @FXML private TableView<?> debtsTable;
+    @FXML private TableColumn<?, ?> saleNumber;
+    @FXML private TableColumn<?, ?> amount;
+    @FXML private TableColumn<?, ?> dateTime;
+    @FXML private TableColumn<?, ?> rest;
+    @FXML private MenuItem excel;
+    @FXML private MenuItem _edit;
+    @FXML private MenuItem delete;
+    @FXML private CustomTextField searchText;
+    @FXML private JFXButton searchBtn;
+    @FXML private AnchorPane formPane;
     @FXML private JFXTextField nameField;
-    @FXML private JFXTextField costField;
     @FXML private JFXTextField percentField;
+    @FXML private JFXTextField costField;
     @FXML private JFXTextArea descriptionArea;
     @FXML private JFXButton save;
-    @FXML private CustomTextField searchText;
-    @FXML private TableView<Payment> taxesTable;
-    @FXML private TableColumn<Payment, String> name;
-    @FXML private TableColumn<Payment, BigDecimal> cost;
-    @FXML private TableColumn<Payment, BigDecimal> percent;
-    @FXML private TableColumn<Payment, Double> description;
-    @FXML private MenuItem excel;
-    @FXML private MenuItem delete;
-    @FXML private AnchorPane formPane;
-    @FXML private MenuItem _edit;
-    @FXML private JFXButton searchBtn;
-    @FXML private SplitPane splitPane;
-
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         setCloseEventHandler();
+        setLanguage();
     }
 
     private void setCloseEventHandler() {
         close.setOnAction(Stages::close);
+    }
+
+    private void setLanguage() {
+        Properties lang = ControlsHandler.getLanguage();
+        title.setText(lang.getProperty("debts"));
     }
 
 
