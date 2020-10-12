757a134cd0f60deb721d3f8139e32ecaeba8f08b
stephen-mafole <stephen-mafole@elikya.tech>
2020-10-12 12:12:00 +0200

    added repository and service for Payment

--- src/main/java/com/elikya/apps/rhoe/desk/repository/PaymentRepository.java
+++ src/main/java/com/elikya/apps/rhoe/desk/repository/PaymentRepository.java
@@ -4,5 +4,8 @@
 
 package com.elikya.apps.rhoe.desk.repository;
 
-public interface PaymentRepository {
+import com.elikya.apps.rhoe.desk.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 }
